32f0b890f5390b129f8ca3208bfe80d285e93df0
baihaqiyazid <baihaqiyazid16@gmail.com>
2022-08-14 05:31:25 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/baihaqiyazid/app-say-hello
+
+go 1.18
+
+require github.com/baihaqiyazid/go-say-hello v1.0.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	go_say_hello "github.com/baihaqiyazid/go-say-hello"
+)
+
+func main() {
+   go_say_hello.SayHello()
+}
